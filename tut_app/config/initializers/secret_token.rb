88f98d4c9b52a7fd2be9f69642133931d6de69d1
Ryan Osten <ryan@classvantage.com>
2014-04-03 09:54:42 -0400

    rails_tut1

---
+++ tut_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TutApp::Application.config.secret_key_base = '59bb3cc007746a37d962f2dbb9430650b4cf6349933e845d045a501c021fc4746118a8fcb5570fd4b878597c7281beac084005107d1ac4110b015ed88b882cc9'
